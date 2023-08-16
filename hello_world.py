27df54e396da8f64dfb105a4d7573b6e3bf9b85f
Nea Parjanen <nea.parjanen@tuni.fi>
2023-08-16 12:22:07 +0300

    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,10 @@
 print("Hello world!")
+<<<<<<< HEAD
 print("Not another hello world")
 print("No")
 print("More")
 print("Hello")
 print("World")
 print("Please, I'm begging you")
+=======
+>>>>>>> 426381d (Initial commit)
