4576514c6a5b3b579c001228b0880a9d7baed686
Nea Parjanen <nea.parjanen@tuni.fi>
2023-08-16 12:22:07 +0300

    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,31 @@
 print("Hello world!")
+<<<<<<< HEAD
+<<<<<<< HEAD
 print("Not another hello world")
 print("No")
+<<<<<<< HEAD
+<<<<<<< HEAD
 print("More")
 print("Hello")
+<<<<<<< HEAD
+<<<<<<< HEAD
 print("World")
 print("Please, I'm begging you")
+<<<<<<< HEAD
+=======
+>>>>>>> 426381d (Initial commit)
+=======
+print("Not another hello world")
+>>>>>>> fbb9ca9 (asd)
+=======
+>>>>>>> 94c4fbf (Line change)
+=======
+print("More")
+>>>>>>> 51905dc (test2)
+=======
+>>>>>>> 1662e16 (commit)
+=======
+print("World")
+>>>>>>> c3cf8d5 (qwerty)
+=======
+>>>>>>> ca63dde (fix typo)
